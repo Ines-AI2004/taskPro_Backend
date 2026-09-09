27e7055e6eff330a408913bf4218c7a7b9ffe4bd
in_mch <imch83832gmail.com>
2026-09-08 16:25:59 -1100

    Add global WebMvcConfigurer CORS fix

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -6,14 +6,7 @@ import com.example.demo.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(
-        origins = {
-                "http://localhost:5173",
-                "https://task-managment-system-g6eh.vercel.app"
-        },
-        allowedHeaders = "*",
-        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS}
-)
+@CrossOrigin(origins = "https://task-managment-system-g6eh.vercel.app")
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
