50d8c8e99d98ef96176015a2d258fe5e7d6f839d
in_mch <imch83832gmail.com>
2026-09-08 15:29:40 -1100

    Allow Vercel domain in CORS policy

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = {
         "http://localhost:5173",
-        "https://task-managment-system-g6eh.vercel.app" // استبدليه برابط Vercel الحقيقي
+        "https://task-managment-system-g6eh.vercel.app" 
 })
 @RestController
 @RequestMapping("/tasks")
